d5eddfc630ec970cb7f432411bfdd2f1b6057b0c
maicodedev@gmail.com <maicodedev@gmail.com>
2024-07-01 19:34:21 +0200

    Resuelta parte II

--- src/js/counter.js
+++ src/js/counter.js
@@ -19,3 +19,17 @@ function decrease(){
 };
 
 export {add, reset, decrease}
+
+
+/* PARTE II
+function decrease() {
+    let numberElement = document.getElementById("number");
+    let currentValue = parseInt(numberElement.innerText);
+
+    if (currentValue > 0) {
+        numberElement.innerText = currentValue - 1;
+    } else {
+
+    }
+}
+ */
