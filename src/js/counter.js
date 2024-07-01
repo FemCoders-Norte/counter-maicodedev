8f0dce22d717b611b366f4357694b93ac23f0a92
maicodedev@gmail.com <maicodedev@gmail.com>
2024-07-01 19:25:15 +0200

    Resuelta parte I

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,21 @@
 function add(){
     //Escribir tu código que aumenta
+    let numberElement = document.getElementById("number");
+    let currentValue = parseInt(numberElement.innerText);
+    numberElement.innerText = currentValue + 1;
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    let numberElement = document.getElementById("number");
+    numberElement.innerText = 0;
 }
 
 function decrease(){
     //Escribir tu código que resta
+    let numberElement = document.getElementById("number");
+    let currentValue = parseInt(numberElement.innerText);
+    numberElement.innerText = currentValue - 1;
 };
 
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -2,3 +2,6 @@ import {add, reset, decrease} from './counter.js'
 
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+document.getElementById("add").addEventListener("click", add);
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("decrease").addEventListener("click", decrease);
